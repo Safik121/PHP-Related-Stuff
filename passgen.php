45fe37624ccfd9e19755454d5c34b79f7c9a6d60
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-04-11 11:00:56 +0200

    Update passgen.php

--- passgen.php
+++ passgen.php
@@ -1,3 +1,4 @@
+<?php
 function passgen(){
     $length = rand(14, 22);
     $number = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
@@ -12,3 +13,4 @@ function passgen(){
     $password = substr(implode($characters), $startingpoint, $length);
     return $password;
 }
+?>
