26d5ca54f34cf286fc190dd58b0b56b310e5784e
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-12-07 09:26:24 +0100

    Update login.php

--- Web_Server/loginApp/login.php
+++ Web_Server/loginApp/login.php
@@ -1,45 +1,83 @@
 <?php
 session_start();
-session_destroy();
 
 $login_form = file_get_contents("login.html");
 $logged_form = file_get_contents("logged.html");
-$username = $_POST['username']??null;
-$password = $_POST['password']??null;
+$changepass_form = file_get_contents("changepass.html");
 
-$users = array("Martin"=>"Martinek123", "Max"=>"Maxicek123", "Dan"=>"Danicek123");
-
-function login($jmeno, $heslo, $uzivatele){
+function login($jmeno, $heslo)
+{
+    $file = file("dataInfo.txt");
     $logged = false;
-    $username = $jmeno;
-    $password = $heslo;
-    $users = $uzivatele;
-    if ($username == null or $password == null) {
-        echo("Žádné info nezadáno");
-    }else {
-        foreach ($users as $user => $value) {
-           if($username == $user && $password == $value){
-            $logged = true;
-            break;
-           }
-           else{
-            $logged = false;
-           }
+
+    if ($jmeno == null || $heslo == null) {
+        echo "Žádné info nezadáno";
+    } else {
+        foreach ($file as $value) {
+            $userInfo = explode(":", $value);
+            $userName = $userInfo[0];
+            $passWord = isset($userInfo[1]) ? $userInfo[1] : '';
+            $perms = isset($userInfo[2]) ? $userInfo[2] : '';
+
+            if ($jmeno === trim($userName) && $heslo === trim($passWord)) {
+                $logged = true;
+                break;
+            }
         }
     }
     return $logged;
 }
 
-function changePass($jmeno, $heslo, $noveHeslo){
-    
+function changePass($jmeno, $heslo, $noveHeslo)
+{
+    $file = file("dataInfo.txt");
+    $vysledek = [];
+
+    foreach ($file as $key) {
+        $userInfo = explode(":", $key);
+        $userName = $userInfo[0];
+        $passWord = isset($userInfo[1]) ? trim($userInfo[1]) : '';
+        $perms = isset($userInfo[2]) ? trim($userInfo[2]) : '';
+
+        if ($jmeno == $userName and $heslo == $passWord) {
+            $passWord = $noveHeslo;
+
+            $vysledek[] = $userName . ":" . $passWord . ":" .$perms ."\n";
+        } else {
+            $vysledek[] = $key;
+        }
+    }
+    file_put_contents("dataInfo.txt", implode("", $vysledek));
 }
 
-if(login($username, $password, $users) == true){
-    $logged_form = str_replace('#username#', $username, $logged_form);
-    echo($logged_form);
-}else{
-    echo($login_form);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['loginn'])) {
+        if (login($_POST['username'], $_POST['password'])) {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['password'] = $_POST['password'];
+            $logged_form = str_replace('#username#', $_SESSION['username'], $logged_form);
+            echo $logged_form;
+        } else {
+            echo $login_form;
+        }
+    } elseif (isset($_POST['logoutt'])) {
+        echo $login_form;
+        session_destroy();
+    } elseif (isset($_POST['changepasss'])) {
+        if (!empty($_POST['newpassword'])) {
+            changePass($_SESSION['username'], $_SESSION['password'], $_POST['newpassword']);
+            $_SESSION['password'] = $_POST['newpassword'];
+            echo $login_form;
+    }
+    } elseif (isset($_POST['tochange'])) {
+        echo $changepass_form;
+    } elseif (isset($_POST['bckk'])) {
+        if (isset($_SESSION['username']) && isset($_SESSION['password']) && login($_SESSION['username'], $_SESSION['password'])) {
+            $logged_form = str_replace('#username#', $_SESSION['username'], $logged_form);
+            echo $logged_form;
+        }
+    }
+} else {
+    echo $login_form;
 }
-
-
 ?>
